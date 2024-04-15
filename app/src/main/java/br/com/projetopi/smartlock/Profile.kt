9afd8b6698e50b7f776a7215eb255b989565c5ba
Capovila <viniciusbuzaneli@gmail.com>
2024-04-15 19:37:31 -0300

    Tela profile com bug nome

--- app/src/main/java/br/com/projetopi/smartlock/Profile.kt
+++ app/src/main/java/br/com/projetopi/smartlock/Profile.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -16,7 +17,9 @@ import com.google.firebase.firestore.firestore
 
 class Profile : Fragment() {
     private lateinit var btnLogout: Button
+    private lateinit var btnTeste: Button
     private lateinit var tvUserEmail: TextView
+    private lateinit var tvUserName: TextView
 
     private lateinit var auth: FirebaseAuth
     private lateinit var simpleStorage: SimpleStorage
@@ -28,20 +31,26 @@ class Profile : Fragment() {
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
 
         btnLogout = root.findViewById(R.id.btnLogout)
+        btnTeste = root.findViewById(R.id.btnTeste)
         simpleStorage = SimpleStorage(requireContext())
         tvUserEmail = root.findViewById(R.id.tvUserEmail)
+        tvUserName = root.findViewById(R.id.tvUserName)
 
         auth = Firebase.auth
 
         val user: User = simpleStorage.getUserAccountData()
 
         tvUserEmail.setText(user.email)
-
+        tvUserName.setText(user.name)
 
         btnLogout.setOnClickListener{
             simpleStorage.clearUserAccount()
             auth.signOut()
             startActivity(Intent(requireContext(),LoginActivity::class.java))
+        }
+
+        btnTeste.setOnClickListener {
+            Snackbar.make(btnTeste, user.name.toString(), Snackbar.LENGTH_LONG).show()
         }
 
         return root
