178420b063f9b46189ad3ffe3146d97b9832ac3b
Arthur Martins <amp.parmeggiani@gmail.com>
2024-04-25 09:36:30 -0300

    Modelo de comentarios para o codigo do arquivo AddCard.kt

--- app/src/main/java/br/com/projetopi/smartlock/AddCard.kt
+++ app/src/main/java/br/com/projetopi/smartlock/AddCard.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import br.com.projetopi.smartlock.databinding.FragmentAddCardBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -16,67 +17,59 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 
 class AddCard : Fragment() {
-    private lateinit var btnAdicionar: Button
-    private lateinit var btnBack: ImageView
+    //Definindo e atribuindo as variaveis para o uso do ViewBinding no fragment
+    private var _binding: FragmentAddCardBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var etNumeroCartao: TextInputEditText
-    private lateinit var etCVV: TextInputEditText
-    private lateinit var etData: TextInputEditText
-    private lateinit var etNomeTitular: TextInputEditText
-
-    private lateinit var tlNumeroCartao: TextInputLayout
-    private lateinit var tlCVV: TextInputLayout
-    private lateinit var tlData: TextInputLayout
-    private lateinit var tlNomeTitular: TextInputLayout
-
+    //Definindo varivais que serão iniciadas posteriormente
+    // (simpleStorage: banco de dados offline do celular, db: banco de dados online do firebase)
     private lateinit var simpleStorage: SimpleStorage
-
     private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_add_card, container, false)
+    ): View {
+        //Atribuindo o layout que sera inflado no fragment
+        _binding = FragmentAddCardBinding.inflate(inflater, container, false)
 
+        //Atribuindo a classe SimpleStorage na variavel
         simpleStorage = SimpleStorage(requireContext())
 
+        //Definindo e atribuindo os dados do usuario que esta no SimpleStorage para a variavel user
         val user: User = simpleStorage.getUserAccountData()
 
-        btnAdicionar = root.findViewById(R.id.btnAdicionar)
-        btnBack = root.findViewById(R.id.btnBack)
-
-        etNumeroCartao = root.findViewById(R.id.etNumeroCartao)
-        etCVV = root.findViewById(R.id.etCVV)
-        etData = root.findViewById(R.id.etData)
-        etNomeTitular = root.findViewById(R.id.etNomeTitular)
-
-        tlNumeroCartao = root.findViewById(R.id.tlNumeroCartao)
-        tlCVV = root.findViewById(R.id.tlCVV)
-        tlData = root.findViewById(R.id.tlData)
-        tlNomeTitular = root.findViewById(R.id.tlNomeTitular)
-
+        //Atribuindo o Firebase Firestore usado no projeto na variavel db
         db = Firebase.firestore
 
-        val editTexts = listOf(etNumeroCartao, etCVV, etData, etNomeTitular)
-        val textLayouts = listOf(tlNumeroCartao, tlCVV, tlData, tlNomeTitular)
+        //Definindo e atribuindo as listas de editTexts e textLayouts
+        val editTexts = listOf(binding.etNumeroCartao, binding.etCVV, binding.etData, binding.etNomeTitular)
+        val textLayouts = listOf(binding.tlNumeroCartao, binding.tlCVV, binding.tlData, binding.tlNomeTitular)
 
+        //Faz com que para cada editText junto com seu textLayout
+        // execute a função setOnFocusChangeListenerInputCheck
         editTexts.forEachIndexed { lt, et ->
             setOnFocusChangeListenerInputCheck(et, textLayouts[lt])
         }
 
-        btnBack.setOnClickListener{
+        //Faz com que quando clicado o btnBack, mude o fragment mostrado na MainActivity para o fragment Profile
+        binding.btnBack.setOnClickListener{
             (activity as MainActivity).changeFragment(Profile())
         }
 
-        btnAdicionar.setOnClickListener{
+        //Faz com que quando clicado o btnAdicionar, verifica se todos os campos foram preenchidos, caso nao
+        // tenha sido executa a função showFieldErrors e mostra um snackbar instruindo o usuario, caso todos os
+        // campos tenham sido preenchidos define a variavel card do tipo CreditCard, atribui os dados inseridos,
+        // adiciona no banco de dados na collection "cards" a variavel card como um novo documento, mostra um
+        // toast instruindo o usuario e muda o fragment mostrado na MainActivity para o fragment Profile
+        binding.btnAdicionar.setOnClickListener{
             if(isFilled()) {
                 val card = CreditCard(
                     user.uid.toString(),
-                    etNumeroCartao.text.toString(),
-                    etCVV.text.toString(),
-                    etData.text.toString(),
-                    etNomeTitular.text.toString()
+                    binding.etNumeroCartao.text.toString(),
+                    binding.etCVV.text.toString(),
+                    binding.etData.text.toString(),
+                    binding.etNomeTitular.text.toString()
                 )
                 db.collection("cards").add(card).addOnSuccessListener {
                     Toast.makeText(requireContext(), "Cartão adicionado!", Toast.LENGTH_LONG).show()
@@ -84,11 +77,16 @@ class AddCard : Fragment() {
                 }
             } else {
                 showFieldErrors()
-                Snackbar.make(btnAdicionar, "Preencha todos os campos corretamente", Snackbar.LENGTH_LONG ).show()
+                Snackbar.make(binding.btnAdicionar, "Preencha todos os campos corretamente", Snackbar.LENGTH_LONG ).show()
             }
         }
-        return root
+        return binding.root
     }
+
+    //Faz com que quando executada, recebe pela lista de parametros o editText e o textLayout, define um listener
+    // no editText e quando o foco sai do editText, se nao estiver preenchido, define o textLayout.error
+    // (funcionalidade do m2.material) como "Preencha o campo", caso o editText tenha sido preenchido, define o
+    // textLayout.error como null
     private fun setOnFocusChangeListenerInputCheck(editText: TextInputEditText, textLayout: TextInputLayout) {
         editText.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
@@ -101,14 +99,19 @@ class AddCard : Fragment() {
         }
     }
 
+    //Faz com que quando executada, retorna um boolean True caso todos os editText tenham sido preenchidos, e False
+    // caso um dos editText nao estiver preenchido
     private fun isFilled(): Boolean {
-        return !(etNumeroCartao.text.toString().isEmpty() || etCVV.text.toString().isEmpty() ||
-                etData.text.toString().isEmpty() || etNomeTitular.text.toString().isEmpty())
+        return !(binding.etNumeroCartao.text.toString().isEmpty() || binding.etCVV.text.toString().isEmpty() ||
+                binding.etData.text.toString().isEmpty() || binding.etNomeTitular.text.toString().isEmpty())
     }
 
+    //Faz com que quando executada, percorre a lista de editText e para cada editText verifica se foi preenchido,
+    // caso esteja preenchido o textLayout.error correspondente é atribuido com null, caso contrario atribui o
+    // textLayout.error correspondente com "Preencha o campo"
     private fun showFieldErrors() {
-        val editTexts = listOf(etNumeroCartao, etCVV, etData, etNomeTitular)
-        val textLayouts = listOf(tlNumeroCartao, tlCVV, tlData, tlNomeTitular)
+        val editTexts = listOf(binding.etNumeroCartao, binding.etCVV, binding.etData, binding.etNomeTitular)
+        val textLayouts = listOf(binding.tlNumeroCartao, binding.tlCVV, binding.tlData, binding.tlNomeTitular)
 
         editTexts.forEachIndexed { index, et ->
             if (et.text.toString().isEmpty()) {
