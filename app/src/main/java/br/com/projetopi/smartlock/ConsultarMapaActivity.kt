eff96e1247f045dc5d9a738fcf2437cd29956e86
Capovila <viniciusbuzaneli@gmail.com>
2024-04-26 19:34:11 -0300

    Estilo do mapa mudado

--- app/src/main/java/br/com/projetopi/smartlock/ConsultarMapaActivity.kt
+++ app/src/main/java/br/com/projetopi/smartlock/ConsultarMapaActivity.kt
@@ -37,7 +37,6 @@ class ConsultarMapaActivity : AppCompatActivity() {
         binding = ActivityConsultarMapaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
