665e4246bdfae069ae443fe24b0b5c3c66ff0c5f
Arthur Martins <amp.parmeggiani@gmail.com>
2024-04-26 22:23:26 -0300

    tirado o hard code, ajustado algumas coisas no app

--- app/src/main/java/br/com/projetopi/smartlock/Fragments/Locacoes.kt
+++ app/src/main/java/br/com/projetopi/smartlock/Fragments/Locacoes.kt
@@ -1,5 +1,6 @@
 package br.com.projetopi.smartlock.Fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,30 +18,53 @@ import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class Locacoes : Fragment() {
 
+    //Definindo e atribuindo as variaveis para o uso do ViewBinding no fragment
     private var _binding:FragmentLocacoesBinding? = null
     private val binding get() = _binding!!
 
+    //Definindo varivais que serão iniciadas posteriormente
+    // (simpleStorage: banco de dados offline do celular, db: banco de dados online do firebase)
     private lateinit var simpleStorage: SimpleStorage
     private lateinit var db: FirebaseFirestore
 
+    //Definindo as variaveis que serao iniciadas posteriormente
+    // (rentalID: id da locação que estiver aberta, establishmentID: id do estabelecimento
+    // da locação que estiver aberta, establishmentManagerName nome do gerente do estabelecimento da
+    // locação que estiver aberta
     private lateinit var rentalID: String
     private lateinit var establishmentID: String
     private lateinit var establishmentManagerName: String
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        //Atribuindo o layout que sera inflado no fragment
         _binding = FragmentLocacoesBinding.inflate(inflater, container, false)
 
+        //Atribuindo a classe SimpleStorage na variavel
         simpleStorage = SimpleStorage(requireContext())
-
+        //Definindo e atribuindo os dados do usuario que esta no SimpleStorage para a variavel user
         val user: User = simpleStorage.getUserAccountData()
 
+        //Atribuindo o Firebase Firestore usado no projeto na variavel db
         db = Firebase.firestore
 
+        //Atribuindo a visibilidade do image view como view.GONE (esconder)
         binding.qrcode.visibility = View.GONE
 
+        //Busca documentos dos quais o idUser seja igual o id do usuario guardado no simpleStorage e que
+        // o rentalOpen seja true (locação aberta), caso seja encontrado algum documento, atribui ao
+        // rentalID o id do documento, atribui ao establishmentID o id do lugar que esta a locação,
+        // define e atribui um objeto MultiFormatWriter que cria códigos de barras em diferentes formatos,
+        // define e atribui uma matriz de bits (bitMatrix) que representa o código de barras criado a partir
+        // do ID do aluguel (rentalID) fornecido, o código de barras é do formato QR_CODE e possui dimensões de 300x300 pixels,
+        // define e atribui um objeto BarcodeEncoder que é usado para converter a matriz de bits (bitMatrix) em um bitmap,
+        // cria um bitmap a partir da matriz de bits (bitMatrix) do código de barras, atribuindo a visibilidade do image view
+        // como view.VISIBLE (mostra), atribui o bitmap do qr code ao image view (qrcode), busca um documento da coleção
+        // establishments que tenha o id do documento = establishmentID, atribui à variavel establishmentManagerName o
+        // managerName do estabelecimento, atribui ao text do textView (tvInfo) o texto "Apresente esse QR Code ao gerente" +
+        // o managerName
         db.collection("rentals")
             .whereEqualTo("idUser", user.uid)
             .whereEqualTo("rentalOpen", true)
