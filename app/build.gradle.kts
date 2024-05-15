7d96cb300188dfd0c1ea75c27516d9d7e88e6ed8
Julia D <julia.ds@puccampinas.edu.br>
2024-05-15 18:43:07 -0300

    CAMERA1 atualizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,11 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildscript {
+        dependencies {
+            classpath("com.android.tools.build:gradle:7.1.2")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -60,4 +65,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.camera:camera-camera2:1.1.0-beta2")
+    implementation("androidx.camera:camera-lifecycle:1.1.0-beta2")
+    implementation("androidx.camera:camera-view:1.1.0-beta2")
 }
