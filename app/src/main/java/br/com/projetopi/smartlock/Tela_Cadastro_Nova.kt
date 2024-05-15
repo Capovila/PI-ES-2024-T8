9d2506acb9687910979dde57fb5bfe45b3ed6ec4
arthurfcb <arthurpeixoto2004@gmail.com>
2024-05-15 19:08:47 -0300

    Mudanças parciais telas

---
+++ app/src/main/java/br/com/projetopi/smartlock/Tela_Cadastro_Nova.kt
@@ -0,0 +1,20 @@
+package br.com.projetopi.smartlock
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tela_Cadastro_Nova : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_cadastro_nova)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
